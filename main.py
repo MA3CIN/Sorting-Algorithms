f4416aae165b40ee0789e8631d921912a61b6b43
Marcin Łukasz Ziółkowski <m.l.ziolkowski@stud.elka.pw.edu.pl>
2021-03-21 00:15:56 +0100

    First algorithm

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+from timeit import repeat
+
+
+def BeginTheTest(algorithm,array):
+    setup_code = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+    smtn = f"{algorithm}({array})"
+    times = repeat(setup=setup_code, stmt=smtn, repeat=10, number=10)
+    print(f"Sorting Algorithm {algorithm}. Fastest time : {min(times)}")
+if __name__ == "__main__":
+    TestArray = [randint(0, 1000) for i in range(100)]
+    BeginTheTest(algorithm="sorted", array=TestArray)
