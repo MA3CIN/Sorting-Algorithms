0e7fcb86f777cce18d318c6fdabec645d2d3ec03
Marcin Ziółkowski <62351083+MA3CIN@users.noreply.github.com>
2021-04-02 00:06:08 +0200

    Insertion sort v0.1

---
+++ Insertion.py
@@ -0,0 +1,13 @@
+def insertionSort(Array):
+    for i in range(1,len(Array)):
+        key = Array[i]
+        j = i-1
+        while j>=0 and key < Array[j]:
+            Array[j+1] = Array[j]
+            j -= 1
+        Array[j+1] = key
+
+ExampleArray = [12,11,10,13,5,6]
+insertionSort(ExampleArray)
+for i in range(len(ExampleArray)):
+    print ("% d" % ExampleArray[i])
